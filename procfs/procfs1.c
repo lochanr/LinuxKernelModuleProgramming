608cfbfec54001d526fa0fa97aa53d3faf7914b1
lochanr <119395180+lochanr@users.noreply.github.com>
2024-05-24 17:00:07 +0530

    Update procfs1.c

--- procfs/procfs1.c
+++ procfs/procfs1.c
@@ -11,13 +11,49 @@ MODULE_DESCRIPTION("procfs example in LKM");
 static struct proc_dir_entry *proc_folder;
 static struct proc_dir_entry *proc_file;
 
-static ssize_t procfile_read(struct file *file_pointer, char __user *buffer, size_t buffer_length, loff_t *offset){
-  
+static ssize_t read_proc(struct file *File, char __user *user_buffer, size_t count, loff_t *offs){
+  char text[] = "Hello from a procfs file!\n";
+	int to_copy, not_copied, delta;
+	to_copy = min(count, sizeof(text));
+	not_copied = copy_to_user(user_buffer, text, to_copy);
+	delta = to_copy - not_copied;
+	return delta;
+}
+
+static ssize_t write_proc(struct file *File, const char __user *user_buffer, size_t count, loff_t *offs){
+  int to_copy, not_copied, delta;
+  char text [255];
+  memset(text, 0, sizeof(text));
+	to_copy = min(count, sizeof(text));
+	not_copied = copy_to_user(text, user_buffer, to_copy);
+	delta = to_copy - not_copied;
+	return delta;
+}
+
+static struct proc_ops fops = {
+  .proc_read = read_proc,
+  .proc_write = write_proc,
+};
 
 static int __init ModuleInit(void){
+  proc_folder = proc_mkdir("example", NULL);
+  if(proc_folder == NULL) {
+		printk("Error creating /proc/example\n");
+		return -ENOMEM;
+	}
+  proc_file = proc_create("dummy", 0666, proc_folder, &fops);
+  if(proc_folder == NULL) {
+		printk("Error creating /proc/example/dummy\n");
+		return -ENOMEM;
+	}
+  printk("procfs_test - Created /proc/hello/dummy\n");
+	return 0;
 }
 
 static void __exit ModuleExit(void){
+  printk("Removing /proc/example/dummy\n");
+	proc_remove(proc_file);
+	proc_remove(proc_folder);
 }
 
 module_init(ModuleInit);
